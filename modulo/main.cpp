8fda21907ff783b55edaee235cb6942687943ee6
hellogdut <milujim@icloud.com>
2016-02-20 00:45:29 +0800

    Initial Commit

---
+++ modulo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  modulo
+//
+//  Created by mario on 16/2/20.
+//  Copyright © 2016年 mario. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
